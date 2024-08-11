025194ea1ba30f93e875c645ee37bfab7fac0209
RahulSinghhub <p1273105@gmail.com>
2024-08-11 14:19:21 +0530

    added some dtos and classes

--- backend/medicineApp/src/main/java/com/mediQuick/medicineApp/entity/Seller.java
+++ backend/medicineApp/src/main/java/com/mediQuick/medicineApp/entity/Seller.java
@@ -1,7 +1,23 @@
 package com.mediQuick.medicineApp.entity;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
-public class Seller {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@DiscriminatorValue(value = "Seller")
+@PrimaryKeyJoinColumn(name = "userId")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "shopOwner")
+public class Seller extends User {
 	private String shopName;
 	private String gstNo;
 }
